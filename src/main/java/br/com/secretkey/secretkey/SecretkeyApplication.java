911e89bd4e61a434fa01cc1b4c3faf4f287447b5
João Victor Reis <joaovictor.reis2000@gmail.com>
2023-11-01 08:54:22 -0300

    Initial commit

---
+++ src/main/java/br/com/secretkey/secretkey/SecretkeyApplication.java
@@ -0,0 +1,13 @@
+package br.com.secretkey.secretkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretkeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/secretkey/secretkey/SecretkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.secretkey.secretkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
